e65c8c74a6ae4079a128918034c15d04a8076fc9
carly <carlyginzinger@gmail.com>
2021-03-09 20:56:14 -0500

    linked other pages

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MoodService } from './../services/mood.service';
+import { User } from '../models/user.model';
+import { AuthService } from './../services/auth.service';
+// import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,18 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService, private moodService: MoodService) { }
 
+//passing in the user id
+  id: string = "";
   ngOnInit(): void {
+    // this.auth.user$.subscribe(user => {
+    //   this.id = user.uid;
+      // console.log(this.id); YES
+      // this.getUserEntries();
+    // })
   }
 
+// //how to pass info to this page?
+// goToEntryPage() {
+//   this.router.navigate(['/entrypage']);
+// }
 }
 
-// id: string = "";
-//   ngOnInit(): void {
-//     this.auth.user$.subscribe(user => {
-//       this.id = user.uid;
-//       // console.log(this.id); YES
-//       this.getUserEntries();
-//     })
-//   }
+
