c715c5bf6d30e74c6b2f0f2bfb7704d7bbe011f5
qelder <qelder5779@yahoo.com>
2021-03-10 19:13:26 -0500

    stats#

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MoodService } from '../services/mood.service';
 import { Router, ActivatedRoute } from "@angular/router";
 import { Entry } from '../models/entry.model';
+import { AuthService } from './../services/auth.service';
 
 
 
@@ -23,17 +24,35 @@ export class StatsComponent implements OnInit {
 
  
 
-  constructor(private moodService: MoodService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private moodService: MoodService, private router: Router, private route: ActivatedRoute, private auth: AuthService) { }
 
   
     
 
   ngOnInit(): void {
-    this.moodService.getUserStats().subscribe((userEntries: Entry[]) => {
-      this.userEntries = userEntries;
-    });
+  //   this.moodService.getUserStats().subscribe((userEntries: Entry[]) => {
+  //     this.userEntries = userEntries;
+  //   });
+    this.displayEntries();
     
+  // }
   }
+  displayEntries() {
+    let mood: any = "";
+    let entrydate: string = "";
+    let entrytime: string = "";
+    let journalentry: string = "";
+    this.auth.user$.subscribe(user => {
+      this.userId = user.uid;
+      let userID: string = this.userId;
+      this.moodService.getUserEntries(mood, entrydate, entrytime, journalentry, userID).subscribe(result => {
+        this.userEntries = result;
+        console.log(this.userEntries);
+      })
+    })
+  }
+
+
 
   getSelectedItem(item){
     console.log('selected items : ',item)
