6f38c6483c191a593d47b96155f2633af27ce6ec
carly <carlyginzinger@gmail.com>
2021-03-10 21:01:51 -0500

    got user entries to display

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MoodService } from './../services/mood.service';
 import { User } from '../models/user.model';
 import { AuthService } from './../services/auth.service';
-// import { Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,23 +10,41 @@ import { AuthService } from './../services/auth.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  public userEntries = [];
+  id: string = "";
 
-  constructor(public auth: AuthService, private moodService: MoodService) { }
+  // constructor(public auth: AuthService, private moodService: MoodService) { } 
+  constructor(public auth: AuthService, private moodService: MoodService, public router: Router) { }
 
 //passing in the user id
-  id: string = "";
+
   ngOnInit(): void {
     // this.auth.user$.subscribe(user => {
     //   this.id = user.uid;
       // console.log(this.id); YES
       // this.getUserEntries();
     // })
+    this.displayEntries();
   }
 
-// //how to pass info to this page?
-// goToEntryPage() {
-//   this.router.navigate(['/entrypage']);
-// }
+  //Delia's code
+  displayEntries() {
+    let mood: any = "";
+    let entrydate: string = "";
+    let entrytime: string = "";
+    let journalentry: string = "";
+    this.auth.user$.subscribe(user => {
+      this.id = user.uid;
+      let userID: string = this.id;
+      this.moodService.getUserEntries(mood, entrydate, entrytime, journalentry, userID).subscribe(result => {
+        this.userEntries = result;
+        console.log(this.userEntries);
+      })
+    })
+  }
+
+
+  
 }
 
 
