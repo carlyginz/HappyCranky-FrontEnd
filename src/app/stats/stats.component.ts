b6f2d6346c5bce2a72809fffaf8a89b5c5e35b70
qelder <qelder5779@yahoo.com>
2021-03-12 11:33:03 -0500

    stats4

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -80,50 +80,50 @@ export class StatsComponent implements OnInit {
     });
   }
 
-//   sadDaysDidThis() {
-//     let i = 0;
-//     this.sadDays = [];
-//     this.sadActivitiesIds = [];
-//     this.userEntries.forEach(element => {
-//       if (element.mood === 1 || element.mood === 2) {
-//         this.sadDays.push(element.id);
-//       }
-//     });
-//     let newSEId;
-//     let newSAId;
-//     let newSObject: EidAname;
-//     this.sadDays.forEach(element => {
-//       this.moodService.getAllEntryActivitiesPerEntryId(element).subscribe(result => {
-//         if (result.length > 0) {
-//           for (i = 0; i < result.length; i++) {
-//             newSEId = result[i].entry_id;
-//             newSAId = result[i].activity_id;
-//             this.moodService.getActivityNameAndCategory(newSAId).subscribe((newResult: any) => {
-//               newSObject = { aName: newResult.name, aCategory: newResult.category };
-//               this.SadActivitiesNamesandCategories.push(newSObject);
-//             })
-//           }
-//         }
-//       });
-//     });
-//   }
+  sadDaysDidThis() {
+    let i = 0;
+    this.sadDays = [];
+    this.sadActivitiesIds = [];
+    this.userEntries.forEach(element => {
+      if (element.mood === 1 || element.mood === 2) {
+        this.sadDays.push(element.id);
+      }
+    });
+    let newSEId;
+    let newSAId;
+    let newSObject: EidAname;
+    this.sadDays.forEach(element => {
+      this.moodService.getAllEntryActivitiesPerEntryId(element).subscribe(result => {
+        if (result.length > 0) {
+          for (i = 0; i < result.length; i++) {
+            newSEId = result[i].entry_id;
+            newSAId = result[i].activity_id;
+            this.moodService.getActivityNameAndCategory(newSAId).subscribe((newResult: any) => {
+              newSObject = { aName: newResult.name, aCategory: newResult.category };
+              this.SadActivitiesNamesandCategories.push(newSObject);
+            })
+          }
+        }
+      });
+    });
+  }
 
 
-//   getSelectedItem(item) {
-//     console.log('selected items : ', item)
-//     this.selctedUser = this.userDetails.filter((user) => user.displayName.includes(item))
-//   }
+  getSelectedItem(item) {
+    console.log('selected items : ', item)
+    this.selctedUser = this.userDetails.filter((user) => user.displayName.includes(item))
+  }
 
-//   getEntryPage() {
-//     this.router.navigate(['/entrypage']);
-//   }
-// }
+  getEntryPage() {
+    this.router.navigate(['/entrypage']);
+  }
 
-// displayStats() {
-  //   this.moodService.getUserEntries(this.mood.toString(), this.userId).subscribe(result => {
-    //     console.log(result);
-    //   })
-    // }
+
+displayStats() {
+    this.moodService.getUserEntries(this.mood.toString(), this.userId).subscribe(result => {
+        console.log(result);
+      })
+    }
 
 
 
