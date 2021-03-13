24ad72b0ee63b1c229a58b9b3f6ccf7a246eb4e0
deliaturner <deliajturner@gmail.com>
2021-03-13 17:52:34 -0500

    routing and footer

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -35,7 +35,7 @@ export class AuthService {
   async googleSignin() {
     const provider = new firebase.auth.GoogleAuthProvider();
     const credential = await this.afAuth.signInWithPopup(provider);
-    // this.router.navigate(["/feelingtoday"]);
+    this.router.navigate(["/dashboard"]);
     return this.updateUserData(credential.user);
   }
 
